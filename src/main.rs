53c9bc1cc0b291f777fa64299ff9a81955b0bbcf
Kevin Bench <Orion636@proton.me>
2022-06-05 15:44:04 -0400

    Solution is complete, just need to format result into Vec<Vec<String>>

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ fn main() {
    pub fn solve_n_queens(n: i32) -> Vec<Vec<String>> {
         let board = build_board(n);
         let mut result: Vec<Vec<String>> = Vec::new();
-        block_board(board, &result, 0);
+        block_board(board, &mut result, 0);
         return result;
     }
 
@@ -19,7 +19,7 @@ fn main() {
     }
 
     
-    pub fn block_board(mut board: Vec<Vec<String>>, result: &mut Vec<Vec<String>>, pos: usize) {
+    pub fn block_board(mut board: Vec<Vec<String>>, result:  &Vec<Vec<String>>, pos: usize) {
         let mut count = 0;
         while count < board.len() {
             if count > 0 && board[pos][count - 1] == "Q" {
@@ -29,13 +29,16 @@ fn main() {
                 board[pos][count] = "Q".to_string();
                 if pos == board.len() - 1 {
                     let joined: Vec<String> = board.iter().enumerate().map(|(_i, val)| {
-                        println!("5");
-                        println!("{:?}", val);
-                        val.join("")
+                        let new_val: Vec<String> = val.iter().map(|x| if x == "X" {
+                            ".".to_string()
+                        } else {
+                             x.clone()
+                        }).collect();
+                        new_val.join("")
                     }).collect();
-                    result.push(joined);
+                    println!("{:?}", joined);
                 } else {
-                    let new_board = board.clone();
+                    let new_board = update_block(&board.clone(), count as i32, pos as i32);
                     block_board(new_board, result, pos + 1);
                 }
             }
@@ -44,25 +47,27 @@ fn main() {
 
     }
   
-    pub fn update_block(board: &Vec<Vec<String>>, pos: i32) -> Vec<Vec<String>> {
-      let mut counter = 0 as usize;
-      let pos = pos as usize;
-      let new_board: Vec<Vec<String>> = board.iter().enumerate().map(|(_i, _val)| {
-        let mut new_row = vec![".".to_string();board.len()];
-        new_row[pos as usize] = "X".to_string();
-        if counter > 0 && counter <= pos {
-          new_row[(pos - counter)] = "X".to_string();
+    pub fn update_block(board: &Vec<Vec<String>>, pos: i32, row: i32) -> Vec<Vec<String>> {
+      let mut counter = 1;
+      let new_board: Vec<Vec<String>> = board.iter().enumerate().map(|(i, val)| {
+          if i as i32 > row {
+                let mut new_row = board[i].clone();
+                new_row[pos as usize] = "X".to_string();
+            if (pos - counter) >= 0 {
+                new_row[(pos - counter) as usize] = "X".to_string();
+            }
+            if (counter + pos) < board.len() as i32 {
+                new_row[(pos + counter) as usize] = "X".to_string();
+            }
+            counter += 1;
+            new_row
+        }else {
+            val.clone()
         }
-        if counter > 0 && (counter + pos) < board.len() {
-          new_row[pos + counter] = "X".to_string();
-        }
-        counter += 1;
-        new_row
       }).collect();
       new_board
     }
 
-  update_block(&vec![vec!["....".to_string()],vec!["....".to_string()],vec!["....".to_string()],vec!["....".to_string()]], 2);
-  println!("{:?}", solve_n_queens(4));
+  solve_n_queens(4);
 
 }
